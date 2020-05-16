e73b15bf1226796c302efed039976d2cad49ba52
Isaev Aleksey <isaev.alex75311@gmail.com>
2020-05-16 04:51:07 +0300

    HW1

--- hw1/1.js
+++ hw1/1.js
@@ -2,8 +2,8 @@
 // градусах по Фаренгейту. Подсказка: расчёт идёт по формуле:
 // Tf = (9 / 5) * Tc + 32, где Tf – температура по Фаренгейту, Tc – температура по Цельсию
 
-'use strict'
-let cels = prompt('Введите температуру в градумах Цельсия')
-let farentgeit = (9/5) * cels + 32
-alert(farentgeit)
+'use strict';
+let cels = prompt('Введите температуру в градумах Цельсия');
+let farentgeit = (9/5) * cels + 32;
+alert(farentgeit);
 
